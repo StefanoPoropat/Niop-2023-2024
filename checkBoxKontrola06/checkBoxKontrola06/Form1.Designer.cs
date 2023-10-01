4617c58e1557be9e786fb209195d2627cfabc3af
Fiusion <92926501+StefanoPoropat@users.noreply.github.com>
2023-10-01 16:08:15 +0200

    Add files via upload

---
+++ checkBoxKontrola06/checkBoxKontrola06/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace checkBoxKontrola06
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBox = new System.Windows.Forms.TextBox();
+            this.chkBreakfast = new System.Windows.Forms.CheckBox();
+            this.chkLunch = new System.Windows.Forms.CheckBox();
+            this.chkDinner = new System.Windows.Forms.CheckBox();
+            this.chkOrder = new System.Windows.Forms.CheckBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBox
+            // 
+            this.txtBox.Location = new System.Drawing.Point(439, 118);
+            this.txtBox.Multiline = true;
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(266, 148);
+            this.txtBox.TabIndex = 0;
+            this.txtBox.Text = "Odabrali ste:\r\n";
+            // 
+            // chkBreakfast
+            // 
+            this.chkBreakfast.AutoSize = true;
+            this.chkBreakfast.Location = new System.Drawing.Point(105, 116);
+            this.chkBreakfast.Name = "chkBreakfast";
+            this.chkBreakfast.Size = new System.Drawing.Size(80, 20);
+            this.chkBreakfast.TabIndex = 1;
+            this.chkBreakfast.Text = "Doručak";
+            this.chkBreakfast.UseVisualStyleBackColor = true;
+            // 
+            // chkLunch
+            // 
+            this.chkLunch.AutoSize = true;
+            this.chkLunch.Location = new System.Drawing.Point(105, 176);
+            this.chkLunch.Name = "chkLunch";
+            this.chkLunch.Size = new System.Drawing.Size(68, 20);
+            this.chkLunch.TabIndex = 2;
+            this.chkLunch.Text = "Ručak";
+            this.chkLunch.UseVisualStyleBackColor = true;
+            // 
+            // chkDinner
+            // 
+            this.chkDinner.AutoSize = true;
+            this.chkDinner.Location = new System.Drawing.Point(105, 246);
+            this.chkDinner.Name = "chkDinner";
+            this.chkDinner.Size = new System.Drawing.Size(73, 20);
+            this.chkDinner.TabIndex = 3;
+            this.chkDinner.Text = "Večera";
+            this.chkDinner.UseVisualStyleBackColor = true;
+            // 
+            // chkOrder
+            // 
+            this.chkOrder.AutoSize = true;
+            this.chkOrder.Location = new System.Drawing.Point(516, 357);
+            this.chkOrder.Name = "chkOrder";
+            this.chkOrder.Size = new System.Drawing.Size(121, 20);
+            this.chkOrder.TabIndex = 4;
+            this.chkOrder.Text = "Sakrij narudžbu";
+            this.chkOrder.UseVisualStyleBackColor = true;
+            this.chkOrder.CheckedChanged += new System.EventHandler(this.chkOrder_CheckedChanged);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(89, 330);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(179, 47);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "Pošalji";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.chkOrder);
+            this.Controls.Add(this.chkDinner);
+            this.Controls.Add(this.chkLunch);
+            this.Controls.Add(this.chkBreakfast);
+            this.Controls.Add(this.txtBox);
+            this.Name = "Form1";
+            this.Text = "Upotreba ChekBox kontrole";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBox;
+        private System.Windows.Forms.CheckBox chkBreakfast;
+        private System.Windows.Forms.CheckBox chkLunch;
+        private System.Windows.Forms.CheckBox chkDinner;
+        private System.Windows.Forms.CheckBox chkOrder;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ checkBoxKontrola06/checkBoxKontrola06/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace checkBoxKontrola06
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            txtBox.Clear();
+            txtBox.Text = "Odabrali ste: " + "\r\n";
+            txtBox.AppendText("\r\n");
+            if (chkBreakfast.Checked == true)
+            {
+                txtBox.AppendText("Doručak " + "\r\n");
+            }
+            if (chkLunch.Checked == true)
+            {
+                txtBox.AppendText("Ručak" + "\r\n");
+            }
+            if (chkDinner.Checked == true)
+            {
+                txtBox.AppendText("Večera" + "\r\n");
+            }
+        }
+        private void chkOrder_CheckedChanged(object sender, EventArgs e)
+        {
+            if(chkOrder.Checked == true) {
+                txtBox.Visible = false;
+            }
+            if (chkOrder.Checked == false)
+            {
+                txtBox.Visible = true;
+            }
+        }
+    }
+}
