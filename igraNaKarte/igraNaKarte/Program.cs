d2bc1c532d080dc3080b46a8c9339d34aaec80a6
Fiusion <92926501+StefanoPoropat@users.noreply.github.com>
2023-10-01 15:25:31 +0200

    Add files via upload

---
+++ igraNaKarte/igraNaKarte/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace igraNaKarte
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float unos = 0, zbroj = 0;
+            start:  do
+            {
+                Console.Write("Unesi broj u rasponu od 1 do 13: ");
+                unos = (float) Convert.ToDouble(Console.ReadLine());    
+                if (unos >= 1 && unos <= 13) {
+                    zbroj += unos;
+                }
+                else
+                {
+                    Console.WriteLine("Broj nije u rasponu, pokušajte ponovno");
+                    Console.WriteLine("");
+                    goto start;
+                }
+               
+            }while (zbroj < 31);
+            if (zbroj == 31)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Pobijedili ste!");
+                Console.WriteLine("Ukupna vrijednost karata je 31");
+            }
+           if(zbroj > 31)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Izgubili ste");
+                Console.WriteLine("Ukupna vrijednost karata je veća od 31 (" + zbroj + ")");
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
