8c926e0b29c4ad36e23272db1293b98dcec37de1
Fiusion <92926501+StefanoPoropat@users.noreply.github.com>
2023-10-20 19:11:30 +0200

    Add files via upload

---
+++ trackBar-Timer-PictureBox-ImageList-ContextMenu10/contextMenuStrip10/contextMenuStrip10/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace contextMenuStrip10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtL = new System.Windows.Forms.TextBox();
+            this.txtD = new System.Windows.Forms.TextBox();
+            this.lijevoNaDesnoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.desnoNaLijevoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lijevoNaDesnoToolStripMenuItem,
+            this.desnoNaLijevoToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(211, 80);
+            // 
+            // txtL
+            // 
+            this.txtL.ContextMenuStrip = this.contextMenuStrip1;
+            this.txtL.Location = new System.Drawing.Point(56, 189);
+            this.txtL.Name = "txtL";
+            this.txtL.Size = new System.Drawing.Size(282, 22);
+            this.txtL.TabIndex = 1;
+            // 
+            // txtD
+            // 
+            this.txtD.ContextMenuStrip = this.contextMenuStrip1;
+            this.txtD.Location = new System.Drawing.Point(466, 189);
+            this.txtD.Name = "txtD";
+            this.txtD.Size = new System.Drawing.Size(282, 22);
+            this.txtD.TabIndex = 2;
+            // 
+            // lijevoNaDesnoToolStripMenuItem
+            // 
+            this.lijevoNaDesnoToolStripMenuItem.Name = "lijevoNaDesnoToolStripMenuItem";
+            this.lijevoNaDesnoToolStripMenuItem.Size = new System.Drawing.Size(181, 24);
+            this.lijevoNaDesnoToolStripMenuItem.Text = "Lijevo na desno";
+            this.lijevoNaDesnoToolStripMenuItem.Click += new System.EventHandler(this.lijevoNaDesnoToolStripMenuItem_Click);
+            // 
+            // desnoNaLijevoToolStripMenuItem
+            // 
+            this.desnoNaLijevoToolStripMenuItem.Name = "desnoNaLijevoToolStripMenuItem";
+            this.desnoNaLijevoToolStripMenuItem.Size = new System.Drawing.Size(210, 24);
+            this.desnoNaLijevoToolStripMenuItem.Text = "Desno na lijevo";
+            this.desnoNaLijevoToolStripMenuItem.Click += new System.EventHandler(this.desnoNaLijevoToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtD);
+            this.Controls.Add(this.txtL);
+            this.Name = "Form1";
+            this.Text = "Upotreba ContextMenuStrip Kontrole";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem lijevoNaDesnoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem desnoNaLijevoToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtL;
+        private System.Windows.Forms.TextBox txtD;
+    }
+}
+
---
+++ trackBar-Timer-PictureBox-ImageList-ContextMenu10/contextMenuStrip10/contextMenuStrip10/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace contextMenuStrip10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lijevoNaDesnoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txtD.Text = txtL.Text; 
+        }
+
+        private void desnoNaLijevoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txtL.Text = txtD.Text;
+        }
+    }
+}
