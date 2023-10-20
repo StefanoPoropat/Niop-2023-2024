186c4c2863e1c90772336b8713ec81d4a384bbc4
Fiusion <92926501+StefanoPoropat@users.noreply.github.com>
2023-10-20 19:12:21 +0200

    Add files via upload

---
+++ trackBar-Timer-PictureBox-ImageList-ContextMenu10/contextMenuStrip10/pictureBox10/pictureBox10/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace pictureBox10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn1 = new System.Windows.Forms.Button();
+            this.pB1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pB1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn1.Location = new System.Drawing.Point(281, 391);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(221, 30);
+            this.btn1.TabIndex = 3;
+            this.btn1.Text = "Prikazi sliku";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // pB1
+            // 
+            this.pB1.BackColor = System.Drawing.Color.Transparent;
+            this.pB1.Image = ((System.Drawing.Image)(resources.GetObject("pB1.Image")));
+            this.pB1.ImageLocation = "";
+            this.pB1.InitialImage = null;
+            this.pB1.Location = new System.Drawing.Point(46, 12);
+            this.pB1.Name = "pB1";
+            this.pB1.Size = new System.Drawing.Size(706, 366);
+            this.pB1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pB1.TabIndex = 2;
+            this.pB1.TabStop = false;
+            this.pB1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.pB1);
+            this.Name = "Form1";
+            this.Text = "Upotreba PictureBox Kontrole";
+            ((System.ComponentModel.ISupportInitialize)(this.pB1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.PictureBox pB1;
+    }
+}
+
