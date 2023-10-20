db35d8e050b161c67ca9aaf77c121cdc9cbe60d6
Fiusion <92926501+StefanoPoropat@users.noreply.github.com>
2023-10-20 19:17:35 +0200

    Add files via upload

---
+++ trackBar-Timer-PictureBox-ImageList-ContextMenu10/timer10/timer10/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace timer10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblTime.Text = DateTime.Now.ToLongTimeString();
+        }
+    }
+}
