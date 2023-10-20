7aaf5334aed3c9166d1ea0500dc4ac34959972bb
Fiusion <92926501+StefanoPoropat@users.noreply.github.com>
2023-10-20 19:17:59 +0200

    Add files via upload

---
+++ trackBar-Timer-PictureBox-ImageList-ContextMenu10/trackBar10/trackBar10/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace trackBar10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lbl1.Text = "Postavljena vrijednost iznosi: " + bar1.Value.ToString();
+        }
+
+        private void bar1_Scroll(object sender, EventArgs e)
+        {
+            lbl1.Text = "Postavljena vrijednost iznosi: " + bar1.Value.ToString();
+        }
+    }
+}
