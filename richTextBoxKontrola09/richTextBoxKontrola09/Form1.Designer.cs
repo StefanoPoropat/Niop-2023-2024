89d024447b58c79faae14f43addc3aea5cc2d03a
Fiusion <92926501+StefanoPoropat@users.noreply.github.com>
2023-10-20 17:07:29 +0200

    Add files via upload

---
+++ richTextBoxKontrola09/richTextBoxKontrola09/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace richTextBoxKontrola09
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(64, 60);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(106, 42);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Citaj";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(651, 60);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(106, 42);
+            this.btnDel.TabIndex = 1;
+            this.btnDel.Text = "Brisi";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(64, 380);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(106, 42);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Spremi";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.Location = new System.Drawing.Point(64, 119);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(693, 240);
+            this.lbl1.TabIndex = 4;
+            this.lbl1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.btnLoad);
+            this.Name = "Form1";
+            this.Text = "Upotreba RichTextBox kontrole";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.RichTextBox lbl1;
+    }
+}
+
---
+++ richTextBoxKontrola09/richTextBoxKontrola09/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace richTextBoxKontrola09
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            lbl1.LoadFile(@"C:Tempproba.rtf");
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            lbl1.SaveFile(@"C:Tempproba.rtf");
+            MessageBox.Show("Tekst spremljen");
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            lbl1.Clear();
+        }
+    }
+}
