7f1975714b3521c3d55ca6982b6cee897cc7ec7b
Fiusion <92926501+StefanoPoropat@users.noreply.github.com>
2023-10-20 19:16:33 +0200

    Add files via upload

---
+++ trackBar-Timer-PictureBox-ImageList-ContextMenu10/imageList10/imageList10/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace imageList10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn1 = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pB1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pB1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(326, 392);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(163, 46);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "Promijeni sliku";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Img2.jpg");
+            this.imageList1.Images.SetKeyName(1, "Img1.jpg");
+            // 
+            // pB1
+            // 
+            this.pB1.Image = global::imageList10.Properties.Resources.Img1;
+            this.pB1.Location = new System.Drawing.Point(171, 12);
+            this.pB1.Name = "pB1";
+            this.pB1.Size = new System.Drawing.Size(481, 364);
+            this.pB1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pB1.TabIndex = 0;
+            this.pB1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.pB1);
+            this.Name = "Form1";
+            this.Text = "Upotreba ImageList Komponente";
+            ((System.ComponentModel.ISupportInitialize)(this.pB1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pB1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ trackBar-Timer-PictureBox-ImageList-ContextMenu10/imageList10/imageList10/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace imageList10
+{
+    public partial class Form1 : Form
+    {
+        public int br = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            
+            if (br == 0)
+            {
+                pB1.Image = imageList1.Images[0];
+                pB1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            if (br == 1)
+            {
+                pB1.Image = imageList1.Images[1];
+                pB1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            if (br == 2)
+            {
+                br = 0;
+                pB1.Image = imageList1.Images[0];
+                pB1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            br++;
+        }
+    }
+}
