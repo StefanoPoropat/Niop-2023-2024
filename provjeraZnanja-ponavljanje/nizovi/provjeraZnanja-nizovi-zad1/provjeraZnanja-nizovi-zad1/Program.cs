42c09dc9e13836b4bdfb4a028921d01c3fa03a20
Fiusion <92926501+StefanoPoropat@users.noreply.github.com>
2023-11-12 17:36:36 +0100

    Add files via upload

---
+++ provjeraZnanja-ponavljanje/nizovi/provjeraZnanja-nizovi-zad1/provjeraZnanja-nizovi-zad1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace provjeraZnanja_nizovi_zad1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int n = 10;
+            int[] numberDivTwo=new int[n];
+            int[] numberNotDivTwo = new int[n];
+            int temporary = 0;
+            Console.WriteLine("Insert max 10 even and 10 odd numbers. ");
+            int i = 0, divTwo=0, notDivTwo=0;
+
+            while ((temporary != 0 || divTwo < 10 || notDivTwo < 10) && i<20)
+            {
+            start: try
+                {
+                    Console.Write((i + 1) + ". ");
+                    temporary = Convert.ToInt32(Console.ReadLine());
+                    if (temporary % 2 == 0)
+                    {
+                        numberDivTwo[divTwo] = temporary;
+                        i++;
+                        divTwo++;
+                    }
+                    if (temporary % 2 == 1)
+                    {
+                        numberNotDivTwo[notDivTwo] = temporary;
+                        i++;
+                        notDivTwo++;
+                    }
+                    if (temporary == 0)
+                    {
+                        goto end;
+                    }
+                }
+                catch (System.FormatException e)
+                {
+                    Console.WriteLine("Inputed number does not fit set paramaters.\nTryAgain\n"); ;
+                    goto start;
+                }
+                catch (System.IndexOutOfRangeException e)
+                {
+                    goto end;
+                }
+            }
+            end: Console.Write("\nEven numbers: ");
+            foreach (int number in numberDivTwo)
+            {
+                Console.Write(number + ", ");
+            }
+            Console.Write("\nOdd numbers: ");
+            foreach (int number in numberNotDivTwo)
+            {
+                Console.Write(number + ", ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ provjeraZnanja-ponavljanje/nizovi/provjeraZnanja-nizovi-zad2/provjeraZnanja-nizovi-zad2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace provjeraZnanja_nizovi_zad2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many numbers do you want to input in each array?: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int br = 0;
+            int[] niz1 = new int[x];
+            int[] niz2 = new int[x];
+            int call = 0;
+            Console.WriteLine("\nArray 1.");
+            for (int i = 0; i<x; i++)
+            {
+            start1: try
+                {
+                    Console.Write((i + 1) + ". ");
+                    niz1[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    goto start1;
+                }
+            }
+            Console.WriteLine("\nArray 2.");
+            for (int i = 0; i < x ; i++)
+            {
+                start2:  try
+                {
+                    Console.Write((i + 1) + ". ");
+                    niz2[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    goto start2;
+                }
+            }
+            for (int i = 0; i < x; i++)
+            {
+                {
+                    if (niz1[i] == niz2[i])
+                    {
+                        br++;
+                    }
+                }
+            }
+            if (call <= 0)
+            {
+                Console.WriteLine("\n" + br + " inputs match.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
