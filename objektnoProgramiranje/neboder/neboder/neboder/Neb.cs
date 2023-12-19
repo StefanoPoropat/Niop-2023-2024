ee199dfac2bf4c7d03b4a915eae4f9a05ff41ab3
Fiusion <92926501+StefanoPoropat@users.noreply.github.com>
2023-12-19 10:24:47 +0100

    Add files via upload

---
+++ objektnoProgramiranje/neboder/neboder/neboder/Neb.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace neboder
+{
+    internal class Neb
+    {
+        double visina = 0;
+        int brKatova = 0;
+
+        public Neb(double visina, int brKatova)
+        {
+            this.Visina = visina;
+            this.BrKatova = brKatova;
+        }
+        public Neb()
+        {
+            this.Visina = visina;
+            this.BrKatova = brKatova;
+        }
+
+        public double Visina { get => visina; set => visina = value; }
+        public int BrKatova { get => brKatova; set => brKatova = value; }
+
+        public override string ToString()
+        {
+            visina += 10;
+            brKatova += 1;
+            double avgKat = visina / brKatova;
+            string str = "Visina: " + visina + "\nBroj katova: " + brKatova + "\nAvg visina kata: " + avgKat;
+            return str;
+        }
+    }
+}
---
+++ objektnoProgramiranje/neboder/neboder/neboder/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace neboder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Neb Tower = new Neb(200, 100);
+
+            Console.WriteLine(Tower.ToString());
+            Console.ReadKey();
+        }
+    }
+}
