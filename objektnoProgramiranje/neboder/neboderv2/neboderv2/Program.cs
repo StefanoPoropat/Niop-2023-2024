8761fa713d67de4afd2f2dd30759ab3e0390b9b6
Fiusion <92926501+StefanoPoropat@users.noreply.github.com>
2024-01-09 11:24:03 +0100

    Add files via upload

---
+++ objektnoProgramiranje/neboder/neboderv2/neboderv2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace neboderv2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Želite li sami unijeti visinu nebodera i broj katova? (Y/N)");
+            string ans = Console.ReadLine();
+            if (ans == "Y")
+            {
+                neb Neboder = new neb();
+                Console.Write("Visina: ");
+                Neboder.Visina = Convert.ToDecimal(Console.ReadLine());
+                Console.Write("Broj katova: ");
+                Neboder.BrKat = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(Neboder.ToString());
+            }
+            if (ans == "N")
+            {
+                neb Neboder = new neb(200, 100);
+                Neboder.Visina += 10;
+                Neboder.BrKat += 1;
+                Console.WriteLine(Neboder.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ objektnoProgramiranje/neboder/neboderv2/neboderv2/neb.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace neboderv2
+{
+    internal class neb
+    {
+        decimal visina;
+        int brKat;
+
+        public neb(decimal visina, int brKat)
+        {
+            this.visina = visina;
+            this.brKat = brKat;
+        }
+        public neb()
+        {
+            this.visina = visina;
+            this.brKat = brKat;
+        }
+
+        public decimal Visina { get => visina; set => visina = value; }
+        public int BrKat { get => brKat; set => brKat = value; }
+        
+        public override string ToString()
+        {
+            String str = "\nVisina nebodera: " + (Visina) + "\nBroj katova: " + (BrKat) + "\nAvg visina kata: " + (Visina) / (BrKat);
+            return str;
+        }
+    }
+}
