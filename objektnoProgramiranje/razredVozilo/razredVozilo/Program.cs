3e71dcfce7e2d82e29c31568652797a726fd0176
Fiusion <92926501+StefanoPoropat@users.noreply.github.com>
2024-01-09 11:24:45 +0100

    Add files via upload

---
+++ objektnoProgramiranje/razredVozilo/razredVozilo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace razredVozilo
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Vozilo jedan = new Vozilo();
+            Vozilo dva = new Vozilo();
+            jedan.Vrsta = "Motocikl";
+            jedan.BrKot = 2;
+            Console.Write("Koliko iznosi max brzina ovog Motora? ");
+            jedan.setMaxBrzina(Convert.ToDouble(Console.ReadLine()));
+
+            dva.Vrsta = "Automobil";
+            dva.BrKot = 4;
+            dva.setMaxBrzina(jedan.getMaxBrzina());
+            Console.WriteLine(jedan.ToString());
+            Console.WriteLine(dva.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ objektnoProgramiranje/razredVozilo/razredVozilo/Vozilo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace razredVozilo
+{
+    internal class Vozilo
+    {
+        String vrsta;
+        double maxBrzina;
+        int brKot;
+
+        public Vozilo(string vrsta, double maxBrzina, int brKot)
+        {
+            this.vrsta = vrsta;
+            this.maxBrzina = maxBrzina;
+            this.brKot = brKot;
+        }
+        public Vozilo()
+        {
+            this.vrsta = vrsta;
+            this.maxBrzina = maxBrzina;
+            this.brKot = brKot;
+        }
+
+        public string Vrsta { get => vrsta; set => vrsta = value; }
+        public void setMaxBrzina(double maxBrzina)
+        {
+            this.maxBrzina = maxBrzina;
+            if (this.Vrsta == "Automobil")
+            {
+               this.maxBrzina = (this.maxBrzina * 0.3) + maxBrzina;
+            }
+        }
+        public double getMaxBrzina()
+        {
+            return maxBrzina;
+        }
+
+        public int BrKot { get => brKot; set => brKot = value; }
+
+        public override string ToString()
+        {
+            string str = "\nVrsta: " + Vrsta + "\nBroj Kotača: " + brKot + "\nMax brzina: " + maxBrzina + "\n";
+            return str;
+        }
+    }
+}
